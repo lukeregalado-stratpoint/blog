1939917ca21f43bc0eaa105667f8604804005755
Luke Regalado <i_luke.regalado@stratpoint.com>
2026-07-10 13:12:42 +0800

    feat: add admin bar - #11

---
+++ components/AdminBarSlot.tsx
@@ -0,0 +1,7 @@
+import AdminBar from "@/components/AdminBar";
+import { isAuthenticated } from "@/lib/auth";
+
+export default async function AdminBarSlot() {
+  const admin = await isAuthenticated();
+  return admin ? <AdminBar /> : null;
+}
